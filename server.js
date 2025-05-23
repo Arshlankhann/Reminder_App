259903d9440f05a0bf146dde383bf90990c0b6e2
Arshlankhann <arshlank894@gmail.com>
2025-05-23 16:45:12 +0530

    update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/reminder_app',{
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log('Mongodb connected'))
-.catch(err => console,log(err))
+.catch(err => console.log(err))
 
 app.use('/api/reminders', reminderRoutes)
 
